fdae697dda6da91c1f0fe58fade1a816e9133e26
Alex Rouvinez <alexandre.rouvinez@student.unisg.ch>
2018-02-16 14:44:48 +0100

    Fixed another mistake

--- test_square.py
+++ test_square.py
@@ -1,13 +1,12 @@
 import unittest
 import square
 
-class TestProduction(unittest.TestCase):
+class TestSquare(unittest.TestCase):
 
     def test_square_function(self):
         input = [0,1,2,3,4,5,6,7,8,9,10]
         output = [0,1,4,9,16,25,36,49,64,81,100]
         self.assertListEqual(output,square.square(input))
-        self.assertListEqual([0,1,4,9,16,25,36,49,64,81,100],square.square([]))
 
 if __name__ == "__main__":
     unittest.main()
