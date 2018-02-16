04f8d555997c2b8ffe583b449cb648a397a1aa1b
Alex Rouvinez <alexandre.rouvinez@student.unisg.ch>
2018-02-16 15:42:07 +0100

    Bug fix in case z = 0

--- numbers.py
+++ numbers.py
@@ -3,11 +3,17 @@ import math
 def three_numbers(x,y,z):
     a_list = []
     b_list = []
+    if z == 0:
+        return print("You cannot divide by 0!!")
+    elif  x > y:
+        return print("The first number has to be smaller than the second!")
 
-    for i in range(x, y + 1):
-        a_list.append(i)
-        if a_list[i] % z == 0:
-            b_list.append(i)
-        else:
-            continue
+    else:
+        for i in range(x, y + 1):
+            a_list.append(i)
+            if a_list[i] % z == 0:
+                b_list.append(i)
+            else:
+                continue
+
     return len(b_list)
