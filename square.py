549956c7a662aa748051dd3d5a8d670b1f3b9132
Virginie <virginie.cauderay@student.unisg.ch>
2018-02-16 14:42:06 +0100

    Edited from Edit square function

--- square.py
+++ square.py
@@ -1,6 +1,6 @@
 import math
 
-def square_function(a_list):
+def square(a_list):
     square_list = []
     for each_element in range(0,len(a_list)):
         square_list.append(math.pow(each_element,2))
